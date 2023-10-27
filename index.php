434e234a32efcac44584deaadbe90989758a1ada
Jorik-Goris <95848835+Jorik-Goris@users.noreply.github.com>
2023-10-27 10:52:13 +0200

    Update index.php
    fix opmaak

--- index.php
+++ index.php
@@ -2,35 +2,63 @@
 <html>
 <head>
     <title>Custom Apache2 Image</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            padding: 20px;
+            margin: 20px auto;
+            max-width: 600px;
+        }
+        h1 {
+            color: #333;
+            font-size: 24px; /* Increase the font size of h1 */
+        }
+        p {
+            font-size: 18px;
+        }
+        .container-name {
+            font-size: 14px; /* Decrease the font size of the container name */
+            text-align: left;
+            color: #888;
+        }
+    </style>
 </head>
 <body>
-    <?php
-    $host = 'contsql-m1-jg'; // This is the MySQL container hostname
-    $user = 'root'; // Your MySQL username
-    $password = 'abc123!'; // Your MySQL password
-    $database = 'mydb'; // Your MySQL database name
+    <div class="container">
+        <?php
+        $host = 'contsql-m1-jg';
+        $user = 'root';
+        $password = 'abc123!';
+        $database = 'mydb';
 
-    // Create a MySQL connection
-    $conn = new mysqli($host, $user, $password, $database);
+        $conn = new mysqli($host, $user, $password, $database);
 
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-    // Fetch data from the database
-    $sql = "SELECT fullname FROM users"; // Replace with your actual table name
-    $result = $conn->query($sql);
+        $sql = "SELECT fullname FROM users";
+        $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        echo "Container Name: " . gethostname() . "<br>"; // Display the container name
-        echo $row["fullname"] . "<p> has reached milestone 1!!! </p>";
-    } else {
-        echo "No records found in the database.";
-    }
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            echo "<h1>" . $row["fullname"] . " has reached milestone 1!!!</h1>";
+            echo "<p class='container-name'>Container Name: " . gethostname() . "</p>";
+        } else {
+            echo "<p>No records found in the database.</p>";
+        }
 
-    $conn->close();
-    ?>
+        $conn->close();
+        ?>
+    </div>
 </body>
 </html>
